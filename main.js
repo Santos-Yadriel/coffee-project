f55f85629521325dced43a527d20c13cbfd8f7fc
Santos Gonzales <Santoszls3@gmail.com>
2023-11-20 17:06:35 -0600

    added cup

--- main.js
+++ main.js
@@ -82,3 +82,34 @@ tbody.innerHTML = renderCoffees(coffees);
 // submitButton.addEventListener('click', updateCoffees);
 coffeeSearch.addEventListener('input', updateCoffees);
 roastSelection.addEventListener('change', updateCoffees);
+
+
+
+
+// ******************** new style ***************
+
+const coffee_name = document.querySelector(".coffee_name");
+const coffee_filling = document.querySelector(".filling");
+const buttons = document.querySelectorAll("button");
+let current_element = null;
+
+const changeCoffeeType = (selected) => {
+    if (current_element) {
+        current_element.classList.remove("selected");
+        coffee_filling.classList.remove(current_element.id);
+    }
+    current_element = selected;
+    coffee_filling.classList.add(current_element.id);
+    current_element.classList.add("selected");
+    coffee_name.innerText = selected.innerText;
+};
+
+const setActiveType = (element) => {
+    element.toggleClass("selected");
+};
+
+[...buttons].forEach((button) => {
+    button.addEventListener("click", () => {
+        changeCoffeeType(button);
+    });
+});
